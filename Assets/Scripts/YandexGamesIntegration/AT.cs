67e057fcd3e583eede78317dd815ac574b2dd39c
HDHero <leodenx@gmail.com>
2024-03-28 17:14:22 +0100

    import: Plugin YG and yandex integration

---
+++ Assets/Scripts/YandexGamesIntegration/AT.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace YandexGamesIntegration
+{
+    public class AT : MonoBehaviour
+    {
+        public void PageVisible() 
+        {
+            Debug.Log("Игра в фокусе");
+        }
+
+        public void PageNotVisible() 
+        {
+            Debug.Log("Игра НЕ в фокусе");
+        }
+    }
+}
---
+++ Assets/Scripts/YandexGamesIntegration/YandexGameLauncher.cs
@@ -0,0 +1,29 @@
+﻿using Cysharp.Threading.Tasks;
+using Infrastructure.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using YG;
+using Zenject;
+
+namespace YandexGamesIntegration
+{
+    [DefaultExecutionOrder(-1000)]
+    public class YandexGameLauncher : MonoBehaviour
+    {
+        private void Start()
+        {
+            if (YandexGame.Instance == null)
+            {
+                SceneManager.LoadScene(SceneId.InitialScene.ToString());
+                return;
+            }
+            Initialize().Forget();
+        }
+
+        private async UniTaskVoid Initialize()
+        {
+            await UniTask.WaitUntil(() => YandexGame.SDKEnabled);
+            ProjectContext.Instance.EnsureIsInitialized();
+        }
+    }
+}
